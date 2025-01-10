ac8bce9a029d5b0ed8e2aabaa70ad97d9db85a63
miracle iyanu <miracleojonugwamaji@gmail.com>
2025-01-10 22:04:46 +0100

    On branch master  Initial commit  Changes to be committed: 	new file:   makeLove.py 	new file:   sally2b.png
    just startin nothing meaningful here

---
+++ makeLove.py
@@ -0,0 +1,22 @@
+# a simple program to make love files
+# cool file name huh
+from zipfile import ZipFile
+import os
+
+print('what the heck is going on here')
+
+dir = 'test'
+
+filesToZip  = os.listdir(dir)
+
+for file in filesToZip:
+	
+
+
+
+
+
+def zip():
+	with ZipFile('','w') as file:
+		file.write('')
+
