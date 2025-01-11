86b352ccfb21804662f3a97fbaa868b045781d09
miracle iyanu <miracleojonugwamaji@gmail.com>
2025-01-11 21:59:06 +0100

    On branch master  Changes to be committed: 	modified:   makeLove.py
     Changes not staged for commit:
    	deleted:    scrot.png
    
    okay now added another function to rename the it to .love, in a nutshell it can now zip and rename. time to polish stuffs up :)

--- makeLove.py
+++ makeLove.py
@@ -2,15 +2,27 @@
 # cool file name huh :)
 import zipfile
 import os
+import shutil
 
 print('what the heck is going on here')
 
+saveTo = ''
+outputName = 'noProb'
+directory = 'test'
+
 def zipAll(directory, outputName):
-    with zipfile.ZipFile(output_filename, 'w') as zipf:
-        for root, dirs, files in os.walk(source_dir):
+    with zipfile.ZipFile(outputName, 'w') as zipf:
+        for root, dirs, files in os.walk(directory):
             for file in files:
                 file_path = os.path.join(root, file)
                 # Add the file to the zip, preserving the directory structure
-                arcname = os.path.relpath(file_path, start=source_dir)
+                arcname = os.path.relpath(file_path, start=directory)
                 zipf.write(file_path, arcname)
     print("zipped the shit :)")
+
+zipAll(directory,outputName)
+
+def rename():
+	shutil.move(outputName,outputName+'.love')
+
+rename()
