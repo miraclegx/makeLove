8e967e1b5ebf55a702b35ee1f5d763581e93982a
miracle iyanu <miracleojonugwamaji@gmail.com>
2025-01-11 21:30:50 +0100

    On branch master  Changes to be committed: 	modified:   makeLove.py 	deleted:    sally2b.png 	new file:   scrot.png
    phew added a function to zip all the files in a directory, yeah thats all :)

--- makeLove.py
+++ makeLove.py
@@ -1,22 +1,16 @@
 # a simple program to make love files
-# cool file name huh
-from zipfile import ZipFile
+# cool file name huh :)
+import zipfile
 import os
 
 print('what the heck is going on here')
 
-dir = 'test'
-
-filesToZip  = os.listdir(dir)
-
-for file in filesToZip:
-	
-
-
-
-
-
-def zip():
-	with ZipFile('','w') as file:
-		file.write('')
-
+def zipAll(directory, outputName):
+    with zipfile.ZipFile(output_filename, 'w') as zipf:
+        for root, dirs, files in os.walk(source_dir):
+            for file in files:
+                file_path = os.path.join(root, file)
+                # Add the file to the zip, preserving the directory structure
+                arcname = os.path.relpath(file_path, start=source_dir)
+                zipf.write(file_path, arcname)
+    print("zipped the shit :)")
