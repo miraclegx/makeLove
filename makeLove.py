3b5419846d0afbfbe0799baec4882aff4453f104
miracle iyanu <miracleojonugwamaji@gmail.com>
2025-01-12 20:20:11 +0100

    On branch with_gui  Changes to be committed: 	new file:   love.png 	modified:   makeLove.py
    okay oo just coming up fuck this shit i am tired

--- makeLove.py
+++ makeLove.py
@@ -3,12 +3,13 @@
 import zipfile
 import os
 import shutil
-
-print('what the heck is going on here')
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
 
 saveTo = ''
-outputName = 'noProb'
-directory = 'test'
+outputName = ''
+mainDirectory = ''
 
 def zipAll(directory, outputName):
     with zipfile.ZipFile(outputName, 'w') as zipf:
@@ -20,9 +21,30 @@ def zipAll(directory, outputName):
                 zipf.write(file_path, arcname)
     print("zipped the shit :)")
 
-zipAll(directory,outputName)
+#zipAll(directory,outputName)
+
+def buildLove():
+	zipAll(mainDirectory+'/','fuck.zip')
+	#rename()
 
 def rename():
-	shutil.move(outputName,outputName+'.love')
+	shutil.move(outputName,outputName+'.love')	
 
-rename()
+def selectDirectory():
+	directory = tk.filedialog.askdirectory()
+	mainDirectory = directory
+
+root = tk.Tk()
+root.geometry('400x400')
+root.title('create main.lua')
+
+loveLogo = tk.PhotoImage(file='love.png')
+loveImageLabel = tk.Label(text='love Logo').place(x=170,y=10)
+#loveImageLabel['image'] = loveLogo
+
+InfoLabel = tk.Label(text='click the button to select the folder').place(x=100,y=70)
+selectButton = tk.Button(text='Click Me :)',command=selectDirectory).place(x=150,y=100)
+
+goAheadButton = tk.Button(text='Build Love',command=buildLove).place(x=150,y=200)
+
+root.mainloop()
